a2d00df56f8db130baab5862b16fd53e7b45f3d2
Edelyne Keisha <114095264+edeliciouso@users.noreply.github.com>
2022-12-21 01:27:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// Add your answer here
+$(function(){
+    $(".lesson").hover(function(){
+        $(this).find(".text-contents").css("display", "inline-block");
+    // when the mouse enters the div
+    }, function(){
+        $(this).find(".text-contents").css("display", "none");
+    }
+    // when the mouse leaves the div
+    );
+});
